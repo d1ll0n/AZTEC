aa3876b0fffe8a70808e6a508ecbec2bf73dea29
Arnaud Schenk <arnaud@creditmint.io>
2019-05-23 17:26:47 +0100

    fix(protocol): remove unused variables

--- packages/protocol/migrations/4_deploy_zk_asset.js
+++ packages/protocol/migrations/4_deploy_zk_asset.js
@@ -19,16 +19,11 @@ module.exports = (deployer, network) => {
         }
         return deployer.deploy(ERC20Mintable).then(({ address: erc20Address }) => {
             const aceAddress = ACE.address;
-            const canAdjustSupply = false;
-            const canConvert = true;
-
             return deployer.deploy(
                 ZkAsset,
                 aceAddress,
                 erc20Address,
                 ERC20_SCALING_FACTOR
-                // canAdjustSupply,
-                // canConvert
             );
         });
     });
